986dbc4ce5035205d1f5126375d3bf77b865383d
CarlRandall <carl.randall@bss.org>
2017-05-04 23:13:49 +0100

    Add project files.

---
+++ AssemblySoft.Serialization/SerializationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblySoft.Serialization
+{
+    /// <summary>
+    /// File client exception
+    /// </summary>
+    class SerializationException : Exception
+    {
+        public SerializationException()
+        {
+        }
+
+        public SerializationException(string message) : base(message)
+        {
+        }
+
+        public SerializationException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+
+}
---
+++ AssemblySoft.Serialization/XmlSerialisationManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace AssemblySoft.Serialization
+{
+
+    public class XmlSerialisationManager<T>
+    {
+        /// <summary>
+        /// Serializes a a collection of objects as xml formatted
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="fileName"></param>
+        public static void SerializeObjects(List<T> items, string fileName)
+        {
+            var path = Path.GetDirectoryName(fileName);
+            if (path != null && !Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var serializer = new XmlSerializer(typeof(List<T>));
+            using (var stream = File.Create(fileName))
+            {
+                serializer.Serialize(stream, items);
+            }
+        }
+
+        /// <summary>
+        /// Deserialises a collection of objects stored as an xml file
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static List<T> DeserializeObjects(string fileName)
+        {
+            var serializer = new XmlSerializer(typeof(List<T>));
+            using (var stream = File.OpenRead(fileName))
+            {
+                try
+                {
+                    var items = (List<T>)(serializer.Deserialize(stream));
+                    return items;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }                
+            }
+        }
+    }
+
+}
